a26e096a6f8bfb6f8df034dc574e4766e2550a35
Pedro Botion <pedrobutii@gmail.com>
2024-08-04 00:25:36 -0300

    testando twilio

--- chat.py
+++ chat.py
@@ -3,6 +3,13 @@
 from twilio.rest import Client
 
 # Autenticação do twilio
+account_sid = "ACccc647a594091aafc40d71bf026728f8"
+auth_token = "d4f328f962fa4860fed3181f65d745a9"
+client = Client(account_sid, auth_token)
 
-account_sid = ACccc647a594091aafc40d71bf026728f8
-
+# Envio de mensagem
+message = client.messages.create(
+    body = "Mensagem automática teste", # mensagem a ser enviada
+    from_= "whatsapp:+14155238886", # Número do Twilio
+    to = "whatsapp:+554498503107" # Número do destinatário
+)

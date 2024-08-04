dc34330e5e3d722724b71ca2c0b445f2244baa8a
Pedro Botion <pedrobutii@gmail.com>
2024-08-04 00:32:55 -0300

    testando api

--- chat.py
+++ chat.py
@@ -1,15 +1,15 @@
-# Chatbot python
+        # Chatbot python
 
-from twilio.rest import Client
+# from twilio.rest import Client
 
-# Autenticação do twilio
-# sid da conta
-# auth_token da conta
-client = Client(sid_conta, auth_token_conta)
+        # Autenticação do twilio
+# account_sid = "sua conta sid"
+# auth_token = "seu auth token"
+# client = Client(account_sid, auth_token)
 
-# Envio de mensagem
-message = client.messages.create(
-    body = "Mensagem automática teste", # mensagem a ser enviada
-    from_= "whatsapp twilio", # Número do Twilio
-    to = "seu whatsapp para teste" # Número do destinatário
-)
+        # Envio de mensagem
+# message = client.messages.create(
+#     body = "Mensagem automática teste", # mensagem a ser enviada
+#     from_= "whatsapp twilio", # Número do Twilio
+#     to = "seu whatsapp" # Número do destinatário
+# )
